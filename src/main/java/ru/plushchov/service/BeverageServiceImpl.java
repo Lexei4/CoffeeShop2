8e39f6766e61f7b2be87114a7c646ac5f54e7a0b
Lexei4 <47830617+Lexei4@users.noreply.github.com>
2020-10-27 20:16:12 +0300

    ApplicationProperties added

--- src/main/java/ru/plushchov/service/BeverageServiceImpl.java
+++ src/main/java/ru/plushchov/service/BeverageServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.plushchov.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import ru.plushchov.dao.BeverageDAO;
 import ru.plushchov.model.Beverage;
@@ -8,7 +10,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Service
+@PropertySource(value = {"classpath:application.properties"})
 public class BeverageServiceImpl implements BeverageService{
+
+    @Value("${beverage.maxIngredients}")
+    private String maxIngredient;
 
     private static final Logger log = LogManager.getLogger(BeverageServiceImpl.class.getName());
 
@@ -21,6 +27,11 @@ public class BeverageServiceImpl implements BeverageService{
 
     @Override
     public void addBeverage(Beverage beverage) {
+
+        if (beverage.getIngredientList().size() >= Integer.valueOf(maxIngredient)){
+            throw new RuntimeException("A beverage can consist only of maximum " + maxIngredient + " ingredients");
+        }
+
         beverageDAO.save(beverage);
     }
 
--- src/main/java/ru/plushchov/service/IngredientServiceImpl.java
+++ src/main/java/ru/plushchov/service/IngredientServiceImpl.java
@@ -1,15 +1,22 @@
 package ru.plushchov.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import ru.plushchov.dao.IngredientDAO;
 import ru.plushchov.model.Ingredient;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Service
+@PropertySource(value = {"classpath:application.properties"})
 public class IngredientServiceImpl implements IngredientService {
+
+    @Value("${ingredient.maxPrice}")
+    private String maxIngredientPrice;
 
     private static final Logger log = LogManager.getLogger(IngredientServiceImpl.class.getName());
 
@@ -19,10 +26,21 @@ public class IngredientServiceImpl implements IngredientService {
         log.info("createService");
         this.ingredientDAO = ingredientDAO;
     }
-
+/**
+addIngridient is a Service method which will only work if price of the ingridient is less than one stipulated in
+ the property file
+ */
     @Override
     public void addIngredient(Ingredient ingredient) {
+
+        BigDecimal price = new BigDecimal("200");
+
+        if (ingredient.getPrice().compareTo(price) > 0)
+        {
+            throw new RuntimeException("A beverage can consist only of maximum " + maxIngredientPrice + " ingredients");
+        }
         log.info("now: " + LocalDateTime.now() +  " IngredientService: " + this.toString());
+
         ingredientDAO.save(ingredient);
     }
 
